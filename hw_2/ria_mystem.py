0d14a43a09d507adfb052fa4feaa8e342a2bc3d0
VeronikaDan <verniknet@list.ru>
2017-03-04 11:13:24 +0300

    Update ria_mystem.py

--- hw_2/ria_mystem.py
+++ hw_2/ria_mystem.py
@@ -1,16 +1,16 @@
 import os
 from subprocess import call
 
-dirs = os.walk('C:/Users/Вероника/Documents/ВШЭ/RIA/plaintexts')
+dirs = os.walk('plaintexts')
 for dir in dirs:
     #дублируем каталог текстов
-    d = os.path.dirname(dir[0])
-    d = d.replace('plaintexts','mystem')
-    os.makedirs(d, exist_ok = True)
+    m_dir = dir[0] + '/'
+    m_dir = m_dir.replace('plaintexts','mystem')
+    os.makedirs(os.path.dirname(m_dir), exist_ok=True)
     files = dir[2]
     for file in files:
         input_f = dir[0] + '/' + file
-        output_f = d + '/' + file
+        output_f = m_dir + file
         #передаем команду запуска mystem в командную строку
-        mystem_call = 'mystem.exe -id ' + input_f + output_f
+        mystem_call = 'mystem.exe -id ' + input_f + ' ' + output_f
         call(mystem_call)
